769d91896c723752c968ace7e9ddf21e85a6d89b
JUDHI3134 <judhistirbehera241@gmail.com>
2024-10-23 08:29:56 +0530

    add routes in our project

---
+++ client/src/pages/BuyCreadit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BuyCreadit = () => {
+  return (
+    <div>
+      rgeg
+    </div>
+  )
+}
+
+export default BuyCreadit
---
+++ client/src/pages/Result.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Result = () => {
+  return (
+    <div>
+      rterte
+    </div>
+  )
+}
+
+export default Result
