408ff49a264d54226d986423b41792ce39227a54
JUDHI3134 <judhistirbehera241@gmail.com>
2024-10-30 11:07:10 +0530

    create api end point for credit balance

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -66,4 +66,21 @@ const clerkwebhooks = async(req,res)=>{
 }
 
 
-export {clerkwebhooks}
+
+
+//Api controller function to get user available credit data
+
+const userCredits = async (req,res) =>{
+    try {
+       
+        const {clerkId} = req.body
+        const userData = await userModel.findOne({clerkId})
+
+        res.json({success: true, credits: userData.creditBalance})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message}) 
+    }
+}
+
+export {clerkwebhooks, userCredits}
