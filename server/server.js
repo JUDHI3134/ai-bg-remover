90c2813ee0e7722acb7ba1d6ae712ff4dccebf1f
JUDHI3134 <judhistirbehera241@gmail.com>
2024-10-30 09:44:12 +0530

    create database connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import connectDB from './config/mongodb.js'
 
 
 //App config
 const PORT = process.env.PORT || 4000
 const app = express()
+await connectDB()
 
 //Initialize middleware
 app.use(express.json())
